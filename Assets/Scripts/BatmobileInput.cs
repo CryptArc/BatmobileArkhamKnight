99094ba377df4d41c380ac7e63aec77efd011a54
Vishal Bilagi <vishalb98@gmail.com>
2017-09-19 22:21:40 +0530

    Updated Batmobile combat mode and drive mode

---
+++ Assets/Scripts/BatmobileInput.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatmobileInput : MonoBehaviour {
+
+
+    public float accelerationInput;
+    public float turnInput;
+    public bool handbrake;
+
+    public bool combatMode;
+    public Vector3 combatInput;
+	
+
+    void Start()
+    {
+        handbrake = false;
+        combatMode = false;
+    }
+
+	void Update () {
+        accelerationInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+        if (Input.GetKey(KeyCode.Space))
+        {
+            handbrake = true;
+        }
+        else
+        {
+            handbrake = false;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            combatMode = true;
+            combatInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        }
+        else
+        {
+            combatMode = false;
+            combatInput = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/BatmobileMotor.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatmobileMotor : MonoBehaviour {
+
+    private BatmobileInput PlayerInput;
+    private Rigidbody batmobileRB;
+    public WheelCollider FL;
+    public WheelCollider FR;
+    public WheelCollider RL;
+    public WheelCollider RR;
+
+    public float maxTorque;
+    public float maxTurnAngle;
+
+    public float steerFactor;
+
+    public float breakingTorque;
+    
+    private float appliedTorque;
+    private float appliedTurnAngle;
+
+    public float combatMoveSpeed;
+
+    void Start () {
+        PlayerInput = GetComponent<BatmobileInput>();
+        batmobileRB = GetComponent<Rigidbody>();
+	}
+
+
+    void Drive()
+    {
+        if (!PlayerInput.combatMode)
+        {
+            //Adjust steer angle accordingly with speed
+            maxTurnAngle = Mathf.Pow(steerFactor, batmobileRB.velocity.magnitude / 8) * 35;
+
+
+            appliedTorque = maxTorque * PlayerInput.accelerationInput;
+            appliedTurnAngle = maxTurnAngle * PlayerInput.turnInput;
+
+            Debug.Log(batmobileRB.velocity + " " + batmobileRB.velocity.magnitude);
+
+            if (appliedTorque != 0)
+            {
+                FL.motorTorque = appliedTorque;
+                FR.motorTorque = appliedTorque;
+            }
+
+            if (appliedTurnAngle != 0)
+            {
+                FL.steerAngle = appliedTurnAngle;
+                FR.steerAngle = appliedTurnAngle;
+            }
+
+            if (PlayerInput.handbrake)
+            {
+                RL.brakeTorque = breakingTorque;
+                RR.brakeTorque = breakingTorque;
+            }
+            else
+            {
+                RL.brakeTorque = 0;
+                RR.brakeTorque = 0;
+            }
+        }
+    }
+
+    void CombatMovement()
+    {
+        
+        if (PlayerInput.combatMode)
+        {
+            if (batmobileRB.velocity.magnitude > 0)
+            {
+                //RL.brakeTorque = breakingTorque;
+                //RR.brakeTorque = breakingTorque;
+                //FL.brakeTorque = breakingTorque;
+                //FR.brakeTorque = breakingTorque;
+
+                batmobileRB.velocity = Vector3.zero;
+            }
+            else
+            {
+                RL.brakeTorque = 0;
+                RR.brakeTorque = 0;
+                FL.brakeTorque = 0;
+                FR.brakeTorque = 0;
+            }
+            batmobileRB.MovePosition(batmobileRB.transform.position + PlayerInput.combatInput * combatMoveSpeed * Time.fixedDeltaTime);
+        }
+    }
+
+    void FixedUpdate () {
+
+        Drive();
+        CombatMovement();
+
+    }
+}
