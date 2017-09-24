706d4f79064eb47ddf3c50078fa7403e6b399fb2
Vishal Bilagi <vishalb98@gmail.com>
2017-09-24 20:33:56 +0530

    Updated particle effects and weapon sounds

--- Assets/Scripts/BatmobileInput.cs
+++ Assets/Scripts/BatmobileInput.cs
@@ -20,6 +20,10 @@ public class BatmobileInput : MonoBehaviour {
 
     public Animator turret;
 
+    public ParticleSystem weaponFlares;
+    public AudioSource lightFireSound;
+    public AudioSource heavyFireSound;
+
     void Start()
     {
         handbrake = false;
@@ -49,21 +53,47 @@ public class BatmobileInput : MonoBehaviour {
             heavyFire = Input.GetAxisRaw("Heavy Fire") != 0 ? true : false;
             combatInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         }
-        else
+        else if(Input.GetAxisRaw("Combat") == 0)
         {
+            
             accelerationInput = Input.GetAxis("Accel");
             turnInput = Input.GetAxis("Horizontal");
             combatMode = false;
             combatInput = Vector3.zero;
+            turret.SetBool("Fire", false);
+            weaponFlares.Stop();
+            lightFire = false;
+            lightFireSound.Stop();
+            //heavyFireSound.Stop();
         }
 
         if (lightFire)
         {
             turret.SetBool("Fire", true);
+            if (!weaponFlares.isPlaying)
+            {
+                weaponFlares.Play();
+            }
+            if (!lightFireSound.isPlaying)
+            {
+                lightFireSound.Play();
+            }
         }
         else
         {
             turret.SetBool("Fire", false);
+            lightFireSound.Stop();
+        }
+        if (heavyFire)
+        {
+            if (!heavyFireSound.isPlaying)
+            {
+                heavyFireSound.Play();
+            }
+        }
+        else
+        {
+            //heavyFireSound.Stop();
         }
     }
 
